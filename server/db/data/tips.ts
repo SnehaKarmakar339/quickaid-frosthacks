9ce0f27088abcc7294b4251b50476c58a0e7ff99
Snigdha Mistry <150170898+snigdhamistry@users.noreply.github.com>
2025-02-08 03:43:37 +0530

    add tips in backend

---
+++ server/db/data/tips.ts
@@ -0,0 +1,19 @@
+const tips = [
+  "Stay Hydrated",
+  "Eat a Balanced Diet",
+  "Exercise Regularly",
+  "Get Enough Sleep",
+  "Practice Stress Management",
+  "Maintain a Healthy Weight",
+  "Stay Mentally Active",
+  "Avoid Smoking and Excessive Alcohol",
+  "Practice Good Hygiene",
+  "Stay Socially Connected",
+  "Regular Check-ups",
+  "Limit Screen Time",
+  "Practice Mindfulness",
+  "Get Outside",
+  "Stay Positive",
+];
+
+export default tips;
