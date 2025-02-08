50fdfe75537674b8b68e01c7373822d7c8072023
Krishnendu Das <86651039+itskdhere@users.noreply.github.com>
2025-02-08 20:21:40 +0530

    add capacitor.js

---
+++ client/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.turingdevs.quickaid',
+  appName: 'QuickAid',
+  webDir: 'dist'
+};
+
+export default config;
