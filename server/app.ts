db6accb720b8b3ec6074c78739eff73d15695b7c
Krishnendu Das <86651039+itskdhere@users.noreply.github.com>
2025-02-09 06:12:10 +0530

    add docker

--- server/app.ts
+++ server/app.ts
@@ -15,7 +15,7 @@ app.use(
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-app.use(express.static("public"));
+// app.use(express.static("../client/dist"));
 
 import diagnostics from "./routes/diagnostics.routes";
 app.use("/api/v1", diagnostics);
